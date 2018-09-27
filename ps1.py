ccec39df787e4c43f96f04ea078d0c1760b11682
eet172292 <eet172292@iitd.ac.in>
2018-09-27 17:24:43 +0530

    Final

--- ps1.py
+++ ps1.py
@@ -1,3 +1,64 @@
-###### this is the first .py file ###########
+
+out=[]
+
+class cross:	#class definition
+	def __init__(self, data):
+		self.data = data
+		self.top = 0  
+		self.down = 0 
+		self.left = 0
+		self.right = 0
+
+x = input()			# reading the values of n,m
+row = int(x.split(" ")[0])
+col = int(x.split(" ")[1])
+
+mat=[[0 for i in range(col)]for j in range(row)]
+
+for i in range(row):
+	mat[i]=input().split(" ")	#reading cross values and storing it in a matrix
+
+for i in range(row):
+    for j in range(col):	#for each element assigning data members of class
+        mat[i][j]= cross(mat[i][j])
+
+
+for i in range(row):
+	for j in range(col):
+		if(mat[i][j].data == 'S'):	#iterating loop over only smart cells
+			up = i-1				#if smart cell then top will be above element
+			d = i+1					#if smart cell then top will be below element
+			l = j-1					#if smart cell then top will be leftmost element and starting loop
+			r = j+1					#if smart cell then right will be nexr right and starting loop
+			for up in range(i-1,-1,-1):
+				if(mat[up][j].data != 'D'): #iterating till not getting dull cell
+					mat[i][j].top=mat[i][j].top+1	#if smart cell then increment value
+				else:
+					break
+			
+			for down in range(i+1,row):
+				if(mat[down][j].data != 'D'):			#iterating till not getting dull cell
+					mat[i][j].down=mat[i][j].down+1		#if smart cell then increment value
+				else:
+					break
+
+			for l in range(j-1,-1,-1):
+				if(mat[i][l].data != 'D'):				#iterating till not getting dull cell
+					mat[i][j].left=mat[i][j].left+1		#if smart cell then increment value
+				else:
+					break
+
+			for r in range(j+1,col):
+				if(mat[i][r].data != 'D'):				#iterating till not getting dull cell
+					mat[i][j].right=mat[i][j].right+1	#if smart cell then increment value
+				else:
+					break
+
+			temp=min(mat[i][j].top,mat[i][j].down,mat[i][j].left,mat[i][j].right) #finding min
+			out.append(temp);
+
+n=len(out)
+out.sort()
+print(out[n-1]*4+1," ",out[n-2]*4+1)	#printing largest and 2nd largest
 
 ####### write your code here ##########
--- ps2.py
+++ ps2.py
@@ -1,3 +1,56 @@
-###### this is the second .py file ###########
 
-####### write your code here ##########
+def rotate(l, n):			#Function to rotate the list
+    return l[-n:] + l[:-n]
+
+k=input()					#Taking input from user
+k1=int(k.split(" ")[0])
+k2=int(k.split(" ")[1])
+k3=int(k.split(" ")[2])
+encrypt=input()
+
+list1=['a','b','c','d','e','f','g','h','i']	# defining 3 list for each group
+list2=['j','k','l','m','n','o','p','q','r']
+list3=['s','t','u','v','w','x','y','z','_']
+out=[]
+
+list4=[]
+list5=[]
+list6=[]
+for i in range(len(encrypt)):		#reading the input character by character
+	#print(encrypt[i])
+	if encrypt[i] in list1:	#checking character belongs to which list, accordingly adding it
+		list4.append(encrypt[i])
+	if encrypt[i] in list2:
+		list5.append(encrypt[i])
+	if encrypt[i] in list3:
+		list6.append(encrypt[i])
+
+
+list4=rotate(list4,k1)			#list rotated
+list5=rotate(list5,k2)
+list6=rotate(list6,k3)
+
+l1=0
+l2=0
+l3=0
+
+for i in range(len(encrypt)):	
+	if encrypt[i] in list1:		#checking if character belongs to list1
+		#print(list4[l1])
+		out.append(list4[l1])	#if yes then appending rotated char value
+		l1=l1+1					#Incrementing the value of counter to access the elemnet of rotated list
+		#print("l1",l1)
+	if encrypt[i] in list2:		#checking if character belongs to list2
+		#print(list5[l2])
+		out.append(list5[l2])	#if yes then appending rotated char value					
+		l2=l2+1					#Incrementing the value of counter to access the elemnet of rotated list
+		#print("l2",l2)
+	if encrypt[i] in list3:		#checking if character belongs to list3
+		#print(list6[l3])
+		out.append(list6[l3])	#if yes then appending rotated char value
+		l3=l3+1					#Incrementing the value of counter to access the elemnet of rotated list
+		#print("l3",l3)
+
+for i in range(len(out)):
+	print(out[i],end="")
+###### write your code here ##########
